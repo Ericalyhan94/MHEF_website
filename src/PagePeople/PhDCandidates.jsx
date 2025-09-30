9857993b7be32a767d298dbc0dfe94c7fcfd0a8d
Ericalyhan94 <xiaohan.bie@mail.mcgill.ca>
2025-09-30 12:08:38 -0400

    updated sid thesis topic

--- src/PagePeople/PhDCandidates.jsx
+++ src/PagePeople/PhDCandidates.jsx
@@ -27,7 +27,7 @@ const PhDCandidates = [
         name: "Sidharth Sarmah",
         image: Sid ,
 		degree1:"M.Eng, Indian Institute of Technology Gandhinagar, India",
-		research_area: "Finite Element Analysis (FEA) of Hydrogen Embrittlement in Steels, Computational Mechanical Properties of Metals",
+		research_area: "Multi-scale Modeling of Hydrogen-Induced Failure in High-Strength Steels",
 		PhD_research_topic:"Finite Element Analysis (FEA) of Hydrogen Embrittlement in Steels"
     },
 
